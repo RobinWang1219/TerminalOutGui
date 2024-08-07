05fbaa819308c542a32b4c31a51572dd99c30ad3
RobinWang1219 <maxrobinwang@gmail.com>
2024-08-08 00:18:50 +0800

    [Common] Fix windows various warning
    [Changes]
    
    [Author]
    Robin
    
    Change-Id: I59875df8d89c0e824db1052ec121d961cc6a948d

--- Build.bat
+++ Build.bat
@@ -7,15 +7,16 @@ SET WORKING_SPACE=%CD%
 
 ruff format .
 
-pyinstaller --noconfirm --onefile --windowed  ^
-              --icon "%WORKING_SPACE%\app_icon.ico" ^
-              --add-data "%WORKING_SPACE%\app_icon.png;." ^
-              --version-file "%WORKING_SPACE%\TerminalOutGui.ffi" ^
-              --distpath "%WORKING_SPACE%\output" ^
-              --workpath "%WORKING_SPACE%\temp\build" ^
-              --specpath "%WORKING_SPACE%\temp" ^
-              --clean ^
-              "%WORKING_SPACE%\TerminalOutGui.py"
+REM windows detect has various in pyinstaller
+REM pyinstaller --noconfirm --onefile --windowed  ^
+              REM --icon "%WORKING_SPACE%\app_icon.ico" ^
+              REM --add-data "%WORKING_SPACE%\app_icon.png;." ^
+              REM --version-file "%WORKING_SPACE%\TerminalOutGui.ffi" ^
+              REM --distpath "%WORKING_SPACE%\output" ^
+              REM --workpath "%WORKING_SPACE%\temp\build" ^
+              REM --specpath "%WORKING_SPACE%\temp" ^
+              REM --clean ^
+              REM "%WORKING_SPACE%\TerminalOutGui.py"
 
-rmdir /s /q "%WORKING_SPACE%\temp"
+REM rmdir /s /q "%WORKING_SPACE%\temp"
 @echo on
